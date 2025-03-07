ce90091c0bed6d6d4b8dfdfad068213e148366cc
BinhPham0826 <ptcb1012588646@gmail.com>
2025-03-07 19:09:05 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,40 @@
 # Configuration file for the Sphinx documentation builder.
+import sphinx_rtd_theme
 
 # -- Project information
-
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Heart disease prediction with advance technology'
+copyright = '2025, Bình Phạm'
+author = 'Kashish Agrawal'
 
 release = '0.1'
 version = '0.1.0'
 
 # -- General configuration
-
 extensions = [
+    'nbsphinx',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx_rtd_theme',
+    'sphinx_copybutton',
+    'sphinx_gallery.load_style',
 ]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
-intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
+html_static_path = ['_static']
 html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
+highlight_language = 'python3'
 epub_show_urls = 'footnote'
+
+# Set the language to Vietnamese
+language = 'vi'
